be0fd1008a937f970f089c9b2f4e0e81af3b99cb
d4nilevi4 <d4nia14@gmail.com>
2025-03-06 23:43:11 +0300

    add enemy spawner

---
+++ src/Thronefall/Assets/Code/Gameplay/Features/Enemies/Behaviours/EnemySpawner.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using Thronefall.Common;
+using Thronefall.Gameplay.Combat;
+using UnityEngine;
+using Zenject;
+
+namespace Thronefall.Gameplay.Enemies
+{
+    public class EnemySpawner : MonoBehaviour
+    {
+        public List<SpawnData> SpawnData = new();
+        public float StartCooldown;
+        
+        private IEnemyFactory _enemyFactory;
+        private IWeaponFactory _weaponFactory;
+
+        private float _spawnTimer;
+        private int _currentSpawnDataIndex;
+
+        [Inject]
+        private void Construct(
+            IEnemyFactory enemyFactory,
+            IWeaponFactory weaponFactory)
+        {
+            _weaponFactory = weaponFactory;
+            _enemyFactory = enemyFactory;
+        }
+
+        private void Awake()
+        {
+            _spawnTimer = StartCooldown;
+        }
+
+        private void Update()
+        {
+            _spawnTimer -= Time.deltaTime;
+
+            if (_spawnTimer <= 0 && _currentSpawnDataIndex < SpawnData.Count)
+            {
+                SpawnData spawnData = SpawnData[_currentSpawnDataIndex];
+                
+                for (int i = 0; i < spawnData.Count; i++)
+                {
+                    GameEntity enemy = _enemyFactory.CreateEnemy(spawnData.EnemyTypeId, transform.position);
+                    _weaponFactory.CreateWeapon(WeaponTypeId.SmallAxe, enemy.Id, CollisionLayer.Hero.AsMask());
+                }
+
+                _spawnTimer = SpawnData[_currentSpawnDataIndex].Cooldown;
+                _currentSpawnDataIndex++;
+            }
+        }
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.red;
+            
+            Gizmos.DrawSphere(transform.position, 0.2f);
+            
+            Gizmos.color = Color.white;
+        }
+    }
+
+    [Serializable]
+    public struct SpawnData
+    {
+        public EnemyTypeId EnemyTypeId;
+        public int Count;
+        public float Cooldown;
+
+        public SpawnData(EnemyTypeId enemyTypeId, int count, float cooldown)
+        {
+            EnemyTypeId = enemyTypeId;
+            Count = count;
+            Cooldown = cooldown;
+        }
+    }
+}
--- src/Thronefall/Assets/Code/Infrastructure/States/GameStates/BattleEnterState.cs
+++ src/Thronefall/Assets/Code/Infrastructure/States/GameStates/BattleEnterState.cs
@@ -9,20 +9,17 @@ namespace Thronefall.Infrastructure
     public class BattleEnterState : SimpleState
     {
         private readonly IHeroFactory _heroFactory;
-        private readonly IEnemyFactory _enemyFactory;
         private readonly IGameStateMachine _stateMachine;
         private readonly ILevelDataProvider _levelDataProvider;
         private readonly IWeaponFactory _weaponFactory;
 
         public BattleEnterState(
             IHeroFactory heroFactory,
-            IEnemyFactory enemyFactory,
             IGameStateMachine stateMachine, 
             ILevelDataProvider levelDataProvider,
             IWeaponFactory weaponFactory)
         {
             _heroFactory = heroFactory;
-            _enemyFactory = enemyFactory;
             _stateMachine = stateMachine;
             _levelDataProvider = levelDataProvider;
             _weaponFactory = weaponFactory;
@@ -31,17 +28,10 @@ namespace Thronefall.Infrastructure
         protected override void Enter()
         {
             PlaceHero();
-
-            PlaceEnemy();
             
             _stateMachine.Enter<BattleLoopState>();
         }
-
-        private void PlaceEnemy()
-        {
-            GameEntity enemy = _enemyFactory.CreateEnemy(EnemyTypeId.Skeleton, _levelDataProvider.SkeletonStartPoint);
-            _weaponFactory.CreateWeapon(WeaponTypeId.SmallAxe, enemy.Id, CollisionLayer.Hero.AsMask());
-        }
+        
 
         private void PlaceHero()
         {
